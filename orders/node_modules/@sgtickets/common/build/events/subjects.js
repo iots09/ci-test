6258500f8e5842166818c320120182ba09b89e1d
Otto Zemzans <oz@wetipi.com>
2021-05-22 12:29:59 +0300

    init

---
+++ orders/node_modules/@sgtickets/common/build/events/subjects.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Subjects;
+(function (Subjects) {
+    Subjects["TicketCreated"] = "ticket:created";
+    Subjects["TicketUpdated"] = "ticket:updated";
+    Subjects["OrderCreated"] = "order:created";
+    Subjects["OrderCancelled"] = "order:cancelled";
+})(Subjects = exports.Subjects || (exports.Subjects = {}));
---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,4 @@
+import { Message } from 'node-nats-streaming'
+import {Subjects, Listener, TicketCreatedEvent} from '@sgtickets/common'
+import { Ticket } from '../../models/ticket'
+
